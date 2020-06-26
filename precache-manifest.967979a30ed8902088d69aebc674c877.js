eaf92d902b8c4cb33d145bd2d0dcda9564c82a04
nitko12 <nitko12@hotmail.com>
2020-06-26 14:22:14 +0200

    Updates

---
+++ precache-manifest.967979a30ed8902088d69aebc674c877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621f97432150c81b30dbe06da3b3ac4d",
+    "url": "/movie-api-cobe/index.html"
+  },
+  {
+    "revision": "a67a37de6381de927f21",
+    "url": "/movie-api-cobe/static/css/main.82c03821.chunk.css"
+  },
+  {
+    "revision": "8a9857e291c88c0669df",
+    "url": "/movie-api-cobe/static/js/2.15f284f6.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/movie-api-cobe/static/js/2.15f284f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67a37de6381de927f21",
+    "url": "/movie-api-cobe/static/js/main.80400f7e.chunk.js"
+  },
+  {
+    "revision": "5642aa345b4b27a05d5a",
+    "url": "/movie-api-cobe/static/js/runtime-main.9150e6b4.js"
+  }
+]);
